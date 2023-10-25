06c27f17f5e1f7291dcb19e3b4f019992913cfb5
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2023-10-24 18:22:32 -0700

    displaying the cart items

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -2,15 +2,35 @@ import React from 'react'
 import { MdOutlineKeyboardBackspace } from 'react-icons/md'
 import { RiRefreshFill } from "react-icons/ri";
 import { motion } from 'framer-motion'
-import Fi1 from "../img/fi1.png"
+import EmptyCart from "../img/emptyCart.svg"
 import { BiMinus } from 'react-icons/bi';
 import { BiPlus } from 'react-icons/bi';
 import { actionType } from '../context/reducer';
 import { useStateValue } from '../context/StateProvider';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../firebase.config";
 
 const CartContainer = () => {
+    const firebaseAuth = getAuth(app);
+   const provider = new GoogleAuthProvider();
 
-    const [{cartShow}, dispatch] = useStateValue()
+    const [{cartShow,cartItems,user}, dispatch] = useStateValue()
+
+    const login = async () => {
+        if (!user) {
+          const {
+            user: { refreshToken, providerData },
+          } = await signInWithPopup(firebaseAuth, provider);
+          dispatch({
+            type: actionType.SET_USER,
+            user: providerData[0],
+          });
+          localStorage.setItem("user", JSON.stringify(providerData[0]));
+        } else {
+          setIsMenu(!isMenu);
+        }
+      };
+
     const showCart = () =>{
         dispatch({
             type:actionType.SET_CART_SHOW,
@@ -38,16 +58,20 @@ const CartContainer = () => {
         </div>
 
         {/* Buttom Section */}
-        <div className='w-full h-full bg-black rounded-t-[2rem] flex flex-col'>
+        
+        {cartItems && cartItems.length > 0 ?(
+            <div className='w-full h-full bg-black rounded-t-[2rem] flex flex-col'>
             <div className='w-full h-340 md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll'>
                 {/* cartItems */}
-                <div className='w-full p-1 px-2 rounded-lg bg-gray-700 flex items-center gap-2'>
-                    <img src={Fi1} alt="" className='w-20 h-20 max-w-[60px] rounded-full object-contain' />
+                {cartItems && cartItems.map((item)=>{
+                    return(
+                        <div key={item.id} className='w-full p-1 px-2 rounded-lg bg-gray-700 flex items-center gap-2'>
+                    <img src={item.imageURL} alt="" className='w-20 h-20 max-w-[60px] rounded-full object-contain' />
 
                     {/* name section */}
                     <div className='flex flex-col gap-1'>
-                        <p className='text-white'>Cholate Vanilla</p>
-                        <p className='font-semibold'>$8.7</p>
+                        <p className='text-white'>{item.title}</p>
+                        <p className='font-semibold'>{item.price}</p>
                     </div>
 
                     {/* button section */}
@@ -55,12 +79,14 @@ const CartContainer = () => {
                         <motion.div whileTap={{scale:0.75}}>
                             <BiMinus/>
                         </motion.div>
-                            <p className='w-5 h-5 rounded-sm bg-gray-400 flex items-center text-white justify-center'>0</p>
+                            <p className='w-5 h-5 rounded-sm bg-gray-400 flex items-center text-white justify-center'>{item.qty}</p>
                         <motion.div whileTap={{scale:0.75}}>
                            <BiPlus/> 
                         </motion.div>
                     </div>
                 </div>
+                    )
+                })}
             </div>
             {/* carts total section */}
             <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
@@ -82,14 +108,17 @@ const CartContainer = () => {
                 </p>
                 </div>
 
-                <motion.button
-                    whileTap={{ scale: 0.8 }}
-                    type="button"
-                    className="w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg">
-                    Check Out
+               {user ?(
+                 <motion.button
+                 whileTap={{ scale: 0.8 }}
+                 type="button"
+                 className="w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg">
+                 Check Out
                 </motion.button>
+               ):<></>}
 
                 <motion.button
+                    onClick={login}
                     whileTap={{ scale: 0.8 }}
                     type="button"
                     className="w-full p-2 rounded-full bg-gradient-to-tr from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg">
@@ -97,6 +126,12 @@ const CartContainer = () => {
                 </motion.button>
             </div>
         </div>
+        ):<div className="w-full h-full flex flex-col items-center justify-center gap-6">
+            <img src={EmptyCart} className="w-300" alt="" />
+            <p className="text-xl text-textColor font-semibold">
+            Add some items to your cart
+            </p>
+        </div>}
     </motion.div>
   )
 }
